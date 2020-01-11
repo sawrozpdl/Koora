cb5f0d81465da8dbb5547db575e19d0dba7195d2
Saroj Paudyal <Sarojpaudyal123@gmail.com>
2020-01-11 20:21:59 +0545

    update complete

--- src/articles/models/Article.py
+++ src/articles/models/Article.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.conf import settings
 from django.utils.safestring import mark_safe
 from markdown_deux import markdown
+from django.urls import reverse
 
 class ArticleManager(KooraManager):
 
