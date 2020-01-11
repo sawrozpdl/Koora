72a5d620c7f1a92663bffd7af0a3952215165643
psycho-xd76 <58454291+psycho-xd76@users.noreply.github.com>
2020-01-11 22:54:12 +0545

    just to get the updates

--- src/koora/views.py
+++ src/koora/views.py
@@ -9,3 +9,6 @@ def read_file(request,filename):
    file_content = f.read()
    f.close()
    return HttpResponse(file_content, content_type="text/plain")
+
+def search(rquest):
+    return HttpResponse("Jibesh Gandu")
