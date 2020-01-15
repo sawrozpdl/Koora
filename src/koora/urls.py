a83b2efe10941cbdcaba4f0c1683a9a110905ecf
Saroj Paudyal <Sarojpaudyal123@gmail.com>
2020-01-16 00:44:22 +0545

    fix static dir

--- src/koora/urls.py
+++ src/koora/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.conf.urls import include, url
 from django.urls import path
 from .views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls', namespace='articles'), name='articles'),
     path('', index, name='home')
 ]
+
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
