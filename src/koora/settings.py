4eb28cf3b4f145131fcf9bb11d6a568e9a58c763
Saroj Paudyal <Sarojpaudyal123@gmail.com>
2020-01-16 12:37:35 +0545

    static changed to assets

--- src/koora/settings.py
+++ src/koora/settings.py
@@ -149,13 +149,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"), ]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "assets"), ]
 
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
