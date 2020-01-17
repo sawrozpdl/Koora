dd7ac8e6effb58becef4419e2ae456c02c6ff3ac
psycho-xd76 <58454291+psycho-xd76@users.noreply.github.com>
2020-01-17 10:54:42 +0545

    to merge dev

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,18 +1,18 @@
-from django.db import models
-from django.contrib.auth.models import User
-# # Create your models here.
-class userAccounts(models.Model):
-    avatar_url = models.CharField(max_length = 250)
-    has_image = models.BooleanField()
-    first_name = models.CharField(max_length = 30)
-    last_name = models.CharField(max_length = 30)
-    userAccounts = models.OneToOneField(User,on_delete=models.CASCADE)
+# from django.db import models
+# from django.contrib.auth.models import User
+# # # Create your models here.
+# class userAccounts(models.Model):
+#     avatar_url = models.CharField(max_length = 250)
+#     has_image = models.BooleanField()
+#     first_name = models.CharField(max_length = 30)
+#     last_name = models.CharField(max_length = 30)
+#     userAccounts = models.OneToOneField(User,on_delete=models.CASCADE)
 
 
-# #name
-# #kooras
-# #role
+# # #name
+# # #kooras
+# # #role
 
-# #name
-# #kooras
-# #role
+# # #name
+# # #kooras
+# # #role
