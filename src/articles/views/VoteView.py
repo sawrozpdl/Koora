9f8e2a7a7eb613fb1f1180d9e0014421b779424b
Saroj Paudyal <Sarojpaudyal123@gmail.com>
2020-01-26 21:51:49 +0545

    bug fixes

--- src/articles/views/VoteView.py
+++ src/articles/views/VoteView.py
@@ -5,22 +5,28 @@ from articles.models import Article, Tag, Vote
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
 import json
+from utils.decorators import fail_safe
 
 class VoteView(View):
 
     def get(self, request):
         return HttpResponse('What you up to ?')
 
+    fail_safe(for_model=User)
     def post(self, request):
-
         data = json.loads(request.body)
         user_id = data['user_id']
         article_id = data['article_id']
         vote_type = data['vote_type']
 
-        content_type = ContentType.objects.get_for_model(Article)
-        article = Article.objects.get(id=article_id)
-        user = User.objects.get(id=user_id)
+        try:
+            content_type = ContentType.objects.get_for_model(Article)
+            article = Article.objects.get(id=article_id)
+            user = User.objects.get(id=user_id)
+        except:
+            return JsonResponse({
+                'status' : 500
+            })
         
         try:
             vote = Vote.objects.of_instance(article).get(user=user)
